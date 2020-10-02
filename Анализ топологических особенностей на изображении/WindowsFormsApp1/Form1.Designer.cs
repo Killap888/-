9ad05d58da8fb8402f454ca181ab7ecf3291bbf2
Killap888 <72222479+Killap888@users.noreply.github.com>
2020-10-02 06:49:48 +0300

    Add files via upload

---
+++ Анализ топологических особенностей на изображении/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelTimeOfWorking = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelNumberOfZone = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_Do_Some_Magic_Thing = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_load_map = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1354, 727);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelTimeOfWorking);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.labelNumberOfZone);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button_Do_Some_Magic_Thing);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button_load_map);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1346, 701);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // labelTimeOfWorking
+            // 
+            this.labelTimeOfWorking.AutoSize = true;
+            this.labelTimeOfWorking.Location = new System.Drawing.Point(97, 654);
+            this.labelTimeOfWorking.Name = "labelTimeOfWorking";
+            this.labelTimeOfWorking.Size = new System.Drawing.Size(13, 13);
+            this.labelTimeOfWorking.TabIndex = 16;
+            this.labelTimeOfWorking.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 654);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Время работы";
+            // 
+            // labelNumberOfZone
+            // 
+            this.labelNumberOfZone.AutoSize = true;
+            this.labelNumberOfZone.Location = new System.Drawing.Point(97, 629);
+            this.labelNumberOfZone.Name = "labelNumberOfZone";
+            this.labelNumberOfZone.Size = new System.Drawing.Size(13, 13);
+            this.labelNumberOfZone.TabIndex = 14;
+            this.labelNumberOfZone.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 629);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Количество зон";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(605, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(755, 699);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button_Do_Some_Magic_Thing
+            // 
+            this.button_Do_Some_Magic_Thing.Location = new System.Drawing.Point(441, 621);
+            this.button_Do_Some_Magic_Thing.Name = "button_Do_Some_Magic_Thing";
+            this.button_Do_Some_Magic_Thing.Size = new System.Drawing.Size(157, 46);
+            this.button_Do_Some_Magic_Thing.TabIndex = 11;
+            this.button_Do_Some_Magic_Thing.Text = "Запуск поиска";
+            this.button_Do_Some_Magic_Thing.UseVisualStyleBackColor = true;
+            this.button_Do_Some_Magic_Thing.Click += new System.EventHandler(this.button_Do_Some_Magic_Thing_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 609);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_load_map
+            // 
+            this.button_load_map.Location = new System.Drawing.Point(278, 621);
+            this.button_load_map.Name = "button_load_map";
+            this.button_load_map.Size = new System.Drawing.Size(157, 47);
+            this.button_load_map.TabIndex = 9;
+            this.button_load_map.Text = "Загрузка изображения";
+            this.button_load_map.UseVisualStyleBackColor = true;
+            this.button_load_map.Click += new System.EventHandler(this.button_load_map_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1346, 701);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Топологические данные";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(7, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1333, 652);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1378, 742);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label labelTimeOfWorking;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelNumberOfZone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_Do_Some_Magic_Thing;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_load_map;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Анализ топологических особенностей на изображении/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        Bitmap image;
+
+        struct Dot//структура хранения значений точки
+        {
+            public int[,] matrix_of_points;
+            public int numberZone;
+
+            /*public Dot(int[,] matrixOfPoints, int numberOfZone)
+            {
+                matrix_of_points = matrixOfPoints;
+                numberZone = numberOfZone;
+
+            }*/
+
+        }
+
+        private void button_load_map_Click(object sender, EventArgs e)//загружаем изображение
+        {
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG"; 
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image = new Bitmap(ofd.FileName);
+                    pictureBox1.Image = image;
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }      
+
+
+        public void LookSomePaintDots()//функция зон
+        {
+            int pictureMaxWidth = image.Width;
+            int pictureMaxHeight = image.Height;
+            int[,] matrixOfPoints = new int[pictureMaxWidth, pictureMaxHeight];
+            int[,] matrixOfZone = new int[pictureMaxWidth, pictureMaxHeight];
+            int[] matrixOfTopologicalFeatureDescription = new int[256];
+            int numberOfZone = 0;
+            bool firstZone = false;
+
+            DateTime time = DateTime.Now;
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    matrixOfZone[x, y] = 0;
+                }
+            }
+
+            
+
+            for (int stepOfAverage = 0; stepOfAverage < 255; stepOfAverage++)
+            {
+                for (int x = 0; x < image.Width-1; x++)
+                {
+                    for (int y = 0; y < image.Height-1; y++)
+                    {
+                        int red, blue, green, averange;//переменные
+                        red = (image.GetPixel(x, y).R);
+                        green = (image.GetPixel(x, y).G);
+                        blue = (image.GetPixel(x, y).B);
+                        averange = (int)(0.299 * red + 0.587 * green + 0.114 * blue);//средн знач
+                        Color p = Color.FromArgb(255, averange, averange, averange);
+
+                        if (averange == stepOfAverage)
+                        {
+                            matrixOfPoints[x, y] = averange;
+                            matrixOfTopologicalFeatureDescription[stepOfAverage] += averange;                            
+
+                            if (firstZone == true)
+                            {
+                                if (x != 0 && y != 0)
+                                {
+                                    if (matrixOfZone[x, y] < matrixOfZone[x+1, y])
+                                    {
+                                        matrixOfZone[x, y] = matrixOfZone[x+1, y];                                        
+                                    }
+                                    else if (matrixOfZone[x, y] < matrixOfZone[x, y+1])
+                                    {
+                                        matrixOfZone[x, y] = matrixOfZone[x, y+1];
+                                    }
+                                    else if (matrixOfZone[x, y] < matrixOfZone[x - 1, y])
+                                    {
+                                        matrixOfZone[x, y] = matrixOfZone[x - 1, y];
+                                    }
+                                    else if (matrixOfZone[x, y] < matrixOfZone[x, y - 1])
+                                    {
+                                        matrixOfZone[x, y] = matrixOfZone[x, y - 1];
+                                    }
+                                    else
+                                    {
+                                        matrixOfZone[x, y] = numberOfZone;
+                                        numberOfZone++;
+
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                firstZone = true;
+                                numberOfZone++;
+                            }
+
+                        }
+
+
+                    }
+                }
+            }
+
+
+            for(int i=0; i<255; i++)
+            {
+                string s = string.Format("[step: {0} || data: {1}] \n\n\n", i, matrixOfTopologicalFeatureDescription[i]);
+                textBox1.Text += s;
+            }
+
+            labelNumberOfZone.Text = numberOfZone.ToString();
+            TimeSpan sp = DateTime.Now - time;
+            labelTimeOfWorking.Text = sp.ToString();
+
+            dataGridView1.ColumnCount = pictureMaxWidth;
+            dataGridView1.RowCount = pictureMaxHeight;
+            for (int i = 0; i < pictureMaxWidth; i++)
+            {
+                for (int j = 0; j < pictureMaxHeight; j++)
+                {
+                    /*int red, blue, green, averange;//переменные
+                    red = (image.GetPixel(i, j).R);
+                    green = (image.GetPixel(i, j).G);
+                    blue = (image.GetPixel(i, j).B);
+                    averange = (int)(0.299 * red + 0.587 * green + 0.114 * blue);//средн знач
+                    Color p = Color.FromArgb(255, averange, averange, averange);*/
+
+                    dataGridView1.Rows[j].Cells[i].Value = matrixOfZone[i, j];
+                    //dataGridView1.Rows[j].Cells[i].Style.BackColor = p;
+                }
+            }
+
+            pictureBox1.Image = image;
+
+        }
+
+        private void button_Do_Some_Magic_Thing_Click(object sender, EventArgs e)//Маджик!!!!
+        {
+            LookSomePaintDots();
+        }
+    }
+}
